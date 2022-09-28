98ba3fdee41a6fc4904f883cb686cdbc77da7f94
Ewan Slater <ewan.slater@gmail.com>
2022-09-28 19:18:34 +0000

    ping, mint, jibber

--- native-image/containerisation/lab/src/main/java/com/example/demo/DemoApplication.java
+++ native-image/containerisation/lab/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import java.util.UUID;
 
 @SpringBootApplication
 @RestController
@@ -22,6 +25,11 @@ public class DemoApplication {
 
     @RequestMapping(method = RequestMethod.GET, path = "/jibber")
     ResponseEntity<String> jibber() {
+	    return ResponseEntity.ok(j.generate());
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/ping")
+    ResponseEntity<String> ping() {
         WebClient client = WebClient.create("https://httpbin.org");
         ResponseEntity<String> response = client.get()
           .retrieve()
@@ -32,5 +40,22 @@ public class DemoApplication {
 
         String nfv = j.generate();
 	    return ResponseEntity.ok(headers);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/mint")
+    ResponseEntity<String> mint() {
+	    HashMap<String, String> items = new HashMap<>();
+	    items.put("id", UUID.randomUUID().toString());
+	    items.put("verse", j.generate());
+	    try {
+	            String jsonString = new ObjectMapper()
+		      .writeValueAsString(items);
+	            return ResponseEntity.ok(items.get("id"));
+	    }
+	    catch (Exception e) {
+		    return ResponseEntity
+		      .internalServerError()
+		      .body(e.getMessage());
+	    }
     }
 }
