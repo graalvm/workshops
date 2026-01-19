ee6e956a8641805d7954cfae177b4095ab33e231
Olya Gupalo <olga.gupalo@oracle.com>
2026-01-19 21:46:22 +0200

    Upgrade Micronaut webserver to Java 25

--- native-image/micronaut-webserver/build-jar-java-base.sh
+++ native-image/micronaut-webserver/build-jar-java-base.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# Distroless Java 21 (Debian)-provides glibc and other libraries needed by the JDK
+# Distroless Java 25 (Debian)-provides glibc and other libraries needed by the JDK
 docker build --no-cache . -f Dockerfile.distroless-java-base-jar -t webserver:distroless-java-base.jar
